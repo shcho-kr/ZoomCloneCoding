9c87cb54572f4742337fa5ac3e42c8b3e14a6c5b
kyushu1996@gmail.com <103431041+shcho-kr@users.noreply.github.com>
2023-03-14 12:05:25 +0900

    chat app

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,7 +11,33 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 
+// const handleListen = () => console.log("Listening on http://localhost:3000");
+// app.listen(3000, handleListen);
+
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
 
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anonymous";
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from Browser"));
+    socket.on("message", msg => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach(aSocket =>
+                    aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
+    })
+});
+
+server.listen(3000, handleListen);
