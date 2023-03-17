2c9abd177ebd9216808ad95b2ac9a7817d4fd34b
kyushu1996@gmail.com <103431041+shcho-kr@users.noreply.github.com>
2023-03-17 13:00:57 +0900

    web socket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -33,6 +33,11 @@ function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
     socket.send(makeMessage("new_message", input.value));
+
+    // const li = document.createElement("li");
+    // li.innerText = `You: ${input.value}`;
+    // messageList.append(li);
+
     input.value = "";
 }
 
